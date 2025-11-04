44b15d09e1dcda5eab45ff6c9d74e5b9fa1d1f1b
Justice Jones <justice-s-jones@uiowa.edu>
2025-11-03 19:54:12 -0600

    Added templates and css stylesheet. (Steps 12-29)
    Added my name to the website and background color. Also had to solve a flask problem with OneDrive. Completed steps 12-29 in the HW.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # module 10 - Flask Application
 # Justice Jones 11/03/2025
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
